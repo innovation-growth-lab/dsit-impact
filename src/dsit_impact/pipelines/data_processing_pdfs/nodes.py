95cd01a7eac7ef323276e31f47dc60f5f302c7d4
ampudia19 <dampudiavicente@gmail.com>
2024-07-15 15:04:17 +0200

    parallelise process

--- src/dsit_impact/pipelines/data_processing_pdfs/nodes.py
+++ src/dsit_impact/pipelines/data_processing_pdfs/nodes.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.6
 """
 
 import logging
-from typing import Sequence, Tuple, Dict, Union, Callable, Generator
+from typing import Sequence, Tuple
 import scipdf
 import pandas as pd
 from thefuzz import fuzz
@@ -15,24 +15,29 @@ logger = logging.getLogger(__name__)
 
 
 def parse_pdf(
-    pdf: str, parent_title: str, contexts: Sequence[str]
+    oa_id: str, pdf: str, parent_title: str, contexts: Sequence[str]
 ) -> Sequence[Tuple[int, str]]:
     """
-    Parses a PDF document and extracts relevant sections based on the 
+    Parses a PDF document and extracts relevant sections based on the
         parent title and contexts.
 
     Args:
+        id (str): The ID of the parent document.
         pdf (str): The path or content of the PDF document.
         parent_title (str): The title of the parent document.
-        contexts (Sequence[str]): A sequence of context strings to match 
+        contexts (Sequence[str]): A sequence of context strings to match
             against the section text.
 
     Returns:
-        Sequence[Tuple[int, str]]: A sequence of tuples containing the index 
+        Sequence[Tuple[int, str]]: A sequence of tuples containing the index
             and heading of the matched sections.
 
     """
-    article_dict = scipdf.parse_pdf_to_dict(pdf)
+    try:
+        article_dict = scipdf.parse_pdf_to_dict(pdf)
+    except Exception as e: # pylint: disable=broad-except
+        logger.error("Error parsing PDF for %s: %s", parent_title, e)
+        return []
 
     best_match_score = 50
     ref_id = None
@@ -48,12 +53,91 @@ def parse_pdf(
     for i, section in enumerate(article_dict.get("sections", [])):
         section_heading = section.get("heading", "")
         if ref_id in section.get("publication_ref", []):
-            sections.append(tuple([i, section_heading]))
+            sections.append(tuple([oa_id, i, section_heading]))
         else:
-            for context in contexts:
-                # fuzzy ratio a substring of context in section text
-                if fuzz.token_set_ratio(context, section.get("text", "")) > 75:
-                    sections.append(tuple([i, section_heading]))
+            if contexts:
+                for context in contexts:
+                    # fuzzy ratio a substring of context in section text
+                    if fuzz.token_set_ratio(context, section.get("text", "")) > 75:
+                        sections.append(tuple([oa_id, i, section_heading]))
+            else:
+                logger.info("No contexts provided for %s", parent_title)
 
     logger.info("Found %d sections for %s", len(sections), parent_title)
     return sections
+
+
+def get_pdf_content(dataset: pd.DataFrame):
+    """
+    Retrieves the content of PDF files based on the provided dataset.
+
+    Args:
+        dataset (pd.DataFrame): The dataset containing 'id', 'pdf_url',
+            'title', and 'context' columns.
+
+    Returns:
+        list: A list of paper sections extracted from the PDF files.
+    """
+    assert all(
+        dataset.columns.isin(["id", "pdf_url", "title", "context"])
+    ), "The dataset should contain 'id', 'pdf_url', 'title', and 'context' columns."
+
+    inputs = dataset.apply(
+        lambda x: (x["id"], x["pdf_url"], x["title"], x["context"]), axis=1
+    ).tolist()
+
+    # get paper sections
+    sections = Parallel(n_jobs=8, verbose=10)(
+        delayed(parse_pdf)(*input) for input in inputs
+    )
+
+    return sections
+
+
+def get_citation_sections(oa_dataset: pd.DataFrame, s2_dataset: pd.DataFrame):
+    """
+    Retrieves citation sections from the PDFs based on the Semantic Scholar data.
+
+    Args:
+        oa_dataset (pd.DataFrame): The OpenAlex dataset containing IDs.
+        s2_dataset (pd.DataFrame): The Semantic Scholar dataset containing titles.
+
+    Returns:
+        Dict: A dictionary containing the processed citation sections.
+
+    """
+    # drop those with None in pdf_url
+    s2_dataset.dropna(subset=["pdf_url"], inplace=True)
+
+    # keep unique id, titles
+    oa_dataset = oa_dataset.drop_duplicates(subset=["id", "title"])
+
+    # merge the datasets
+    merged_data = pd.merge(oa_dataset, s2_dataset, on="id", how="right")
+
+    # groupby 'id' and 'pdf_url', create a list of contexts
+    merged_data = (
+        merged_data.groupby(["id", "title", "pdf_url"])["context"]
+        .apply(list)
+        .reset_index()
+    )
+
+    # split the dataset into chunks of 1_000
+    dataset_chunks = [
+        merged_data.iloc[i : i + 1_000] for i in range(0, len(merged_data), 1_000)
+    ]
+
+    for i, chunk in enumerate(dataset_chunks):
+        logger.info("Processing chunk %d / %d", i, len(dataset_chunks))
+        # get the PDF content
+        processed_data = get_pdf_content(
+            dataset=chunk,
+        )
+        logger.info("Processed chunk %d / %d", i, len(dataset_chunks))
+
+        processed_df = pd.DataFrame(
+            [item for sublist in processed_data for item in sublist],
+            columns=["id", "section_index", "section_heading"],
+        )
+
+        yield {f"s{i}": processed_df}
--- src/dsit_impact/pipelines/data_processing_pdfs/pipeline.py
+++ src/dsit_impact/pipelines/data_processing_pdfs/pipeline.py
@@ -3,8 +3,19 @@ This is a boilerplate pipeline 'data_processing_pdfs'
 generated using Kedro 0.19.6
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import get_citation_sections
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=get_citation_sections,
+            inputs={
+                "oa_dataset": "pdfs.oa_dataset.input",
+                "s2_dataset": "pdfs.s2_dataset.input",
+            },
+            outputs="s2.section_details.raw",
+            name="get_citation_sections"
+        )
+    ])
